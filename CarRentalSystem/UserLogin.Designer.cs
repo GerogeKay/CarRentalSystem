e0f724330c5680a9a5d4730d1b42aec9fc48e712
GerogeKay <gouhuoinam@qq.com>
2020-05-27 06:35:15 +0800

    用户登录界面

--- CarRentalSystem/UserLogin.Designer.cs
+++ CarRentalSystem/UserLogin.Designer.cs
@@ -30,19 +30,19 @@
         {
             this.LoginPages = new System.Windows.Forms.TabControl();
             this.UserPage = new System.Windows.Forms.TabPage();
-            this.AdminPage = new System.Windows.Forms.TabPage();
-            this.userAccountLabel = new System.Windows.Forms.Label();
-            this.userPwdLabel = new System.Windows.Forms.Label();
-            this.userAccountTextBox = new System.Windows.Forms.TextBox();
-            this.userPwdTextBox = new System.Windows.Forms.TextBox();
-            this.userLoginButton = new System.Windows.Forms.Button();
             this.userQuitButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.userLoginButton = new System.Windows.Forms.Button();
+            this.userPwdTextBox = new System.Windows.Forms.TextBox();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.userPwdLabel = new System.Windows.Forms.Label();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.AdminPage = new System.Windows.Forms.TabPage();
+            this.adminQuitButton = new System.Windows.Forms.Button();
+            this.adminLoginButton = new System.Windows.Forms.Button();
+            this.adminPwdTextBox = new System.Windows.Forms.TextBox();
+            this.adminNameTextBox = new System.Windows.Forms.TextBox();
+            this.adminPwdLabel = new System.Windows.Forms.Label();
+            this.adminNameLabel = new System.Windows.Forms.Label();
             this.LoginPages.SuspendLayout();
             this.UserPage.SuspendLayout();
             this.AdminPage.SuspendLayout();
@@ -63,9 +63,9 @@
             this.UserPage.Controls.Add(this.userQuitButton);
             this.UserPage.Controls.Add(this.userLoginButton);
             this.UserPage.Controls.Add(this.userPwdTextBox);
-            this.UserPage.Controls.Add(this.userAccountTextBox);
+            this.UserPage.Controls.Add(this.userNameTextBox);
             this.UserPage.Controls.Add(this.userPwdLabel);
-            this.UserPage.Controls.Add(this.userAccountLabel);
+            this.UserPage.Controls.Add(this.userNameLabel);
             this.UserPage.Location = new System.Drawing.Point(4, 22);
             this.UserPage.Name = "UserPage";
             this.UserPage.Padding = new System.Windows.Forms.Padding(3);
@@ -73,67 +73,6 @@
             this.UserPage.TabIndex = 0;
             this.UserPage.Text = "用户登录";
             this.UserPage.UseVisualStyleBackColor = true;
-            // 
-            // AdminPage
-            // 
-            this.AdminPage.Controls.Add(this.button1);
-            this.AdminPage.Controls.Add(this.button2);
-            this.AdminPage.Controls.Add(this.textBox1);
-            this.AdminPage.Controls.Add(this.textBox2);
-            this.AdminPage.Controls.Add(this.label1);
-            this.AdminPage.Controls.Add(this.label2);
-            this.AdminPage.Location = new System.Drawing.Point(4, 22);
-            this.AdminPage.Name = "AdminPage";
-            this.AdminPage.Padding = new System.Windows.Forms.Padding(3);
-            this.AdminPage.Size = new System.Drawing.Size(792, 425);
-            this.AdminPage.TabIndex = 1;
-            this.AdminPage.Text = "管理员登录";
-            this.AdminPage.UseVisualStyleBackColor = true;
-            // 
-            // userAccountLabel
-            // 
-            this.userAccountLabel.AutoSize = true;
-            this.userAccountLabel.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.userAccountLabel.Location = new System.Drawing.Point(270, 135);
-            this.userAccountLabel.Name = "userAccountLabel";
-            this.userAccountLabel.Size = new System.Drawing.Size(56, 16);
-            this.userAccountLabel.TabIndex = 0;
-            this.userAccountLabel.Text = "用户名";
-            // 
-            // userPwdLabel
-            // 
-            this.userPwdLabel.AutoSize = true;
-            this.userPwdLabel.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.userPwdLabel.Location = new System.Drawing.Point(270, 188);
-            this.userPwdLabel.Name = "userPwdLabel";
-            this.userPwdLabel.Size = new System.Drawing.Size(56, 16);
-            this.userPwdLabel.TabIndex = 1;
-            this.userPwdLabel.Text = "密  码";
-            // 
-            // userAccountTextBox
-            // 
-            this.userAccountTextBox.Location = new System.Drawing.Point(331, 135);
-            this.userAccountTextBox.Name = "userAccountTextBox";
-            this.userAccountTextBox.Size = new System.Drawing.Size(169, 21);
-            this.userAccountTextBox.TabIndex = 2;
-            // 
-            // userPwdTextBox
-            // 
-            this.userPwdTextBox.Location = new System.Drawing.Point(331, 188);
-            this.userPwdTextBox.Name = "userPwdTextBox";
-            this.userPwdTextBox.PasswordChar = '●';
-            this.userPwdTextBox.Size = new System.Drawing.Size(169, 21);
-            this.userPwdTextBox.TabIndex = 3;
-            // 
-            // userLoginButton
-            // 
-            this.userLoginButton.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.userLoginButton.Location = new System.Drawing.Point(273, 246);
-            this.userLoginButton.Name = "userLoginButton";
-            this.userLoginButton.Size = new System.Drawing.Size(98, 35);
-            this.userLoginButton.TabIndex = 4;
-            this.userLoginButton.Text = "登  录";
-            this.userLoginButton.UseVisualStyleBackColor = true;
             // 
             // userQuitButton
             // 
@@ -145,60 +84,121 @@
             this.userQuitButton.Text = "退  出";
             this.userQuitButton.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // userLoginButton
             // 
-            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(407, 246);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 35);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "退  出";
-            this.button1.UseVisualStyleBackColor = true;
+            this.userLoginButton.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.userLoginButton.Location = new System.Drawing.Point(273, 246);
+            this.userLoginButton.Name = "userLoginButton";
+            this.userLoginButton.Size = new System.Drawing.Size(98, 35);
+            this.userLoginButton.TabIndex = 4;
+            this.userLoginButton.Text = "登  录";
+            this.userLoginButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // userPwdTextBox
             // 
-            this.button2.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(273, 246);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(98, 35);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "登  录";
-            this.button2.UseVisualStyleBackColor = true;
+            this.userPwdTextBox.Location = new System.Drawing.Point(331, 188);
+            this.userPwdTextBox.Name = "userPwdTextBox";
+            this.userPwdTextBox.PasswordChar = '●';
+            this.userPwdTextBox.Size = new System.Drawing.Size(169, 21);
+            this.userPwdTextBox.TabIndex = 3;
             // 
-            // textBox1
+            // userNameTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(331, 188);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PasswordChar = '●';
-            this.textBox1.Size = new System.Drawing.Size(169, 21);
-            this.textBox1.TabIndex = 9;
+            this.userNameTextBox.Location = new System.Drawing.Point(331, 135);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(169, 21);
+            this.userNameTextBox.TabIndex = 2;
             // 
-            // textBox2
+            // userPwdLabel
             // 
-            this.textBox2.Location = new System.Drawing.Point(331, 135);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(169, 21);
-            this.textBox2.TabIndex = 8;
+            this.userPwdLabel.AutoSize = true;
+            this.userPwdLabel.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.userPwdLabel.Location = new System.Drawing.Point(270, 188);
+            this.userPwdLabel.Name = "userPwdLabel";
+            this.userPwdLabel.Size = new System.Drawing.Size(56, 16);
+            this.userPwdLabel.TabIndex = 1;
+            this.userPwdLabel.Text = "密  码";
             // 
-            // label1
+            // userNameLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(270, 188);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 16);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "密  码";
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.userNameLabel.Location = new System.Drawing.Point(270, 135);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(56, 16);
+            this.userNameLabel.TabIndex = 0;
+            this.userNameLabel.Text = "用户名";
             // 
-            // label2
+            // AdminPage
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(270, 135);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 16);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "用户名";
+            this.AdminPage.Controls.Add(this.adminQuitButton);
+            this.AdminPage.Controls.Add(this.adminLoginButton);
+            this.AdminPage.Controls.Add(this.adminPwdTextBox);
+            this.AdminPage.Controls.Add(this.adminNameTextBox);
+            this.AdminPage.Controls.Add(this.adminPwdLabel);
+            this.AdminPage.Controls.Add(this.adminNameLabel);
+            this.AdminPage.Location = new System.Drawing.Point(4, 22);
+            this.AdminPage.Name = "AdminPage";
+            this.AdminPage.Padding = new System.Windows.Forms.Padding(3);
+            this.AdminPage.Size = new System.Drawing.Size(792, 425);
+            this.AdminPage.TabIndex = 1;
+            this.AdminPage.Text = "管理员登录";
+            this.AdminPage.UseVisualStyleBackColor = true;
+            // 
+            // adminQuitButton
+            // 
+            this.adminQuitButton.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.adminQuitButton.Location = new System.Drawing.Point(407, 246);
+            this.adminQuitButton.Name = "adminQuitButton";
+            this.adminQuitButton.Size = new System.Drawing.Size(93, 35);
+            this.adminQuitButton.TabIndex = 11;
+            this.adminQuitButton.Text = "退  出";
+            this.adminQuitButton.UseVisualStyleBackColor = true;
+            // 
+            // adminLoginButton
+            // 
+            this.adminLoginButton.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.adminLoginButton.Location = new System.Drawing.Point(273, 246);
+            this.adminLoginButton.Name = "adminLoginButton";
+            this.adminLoginButton.Size = new System.Drawing.Size(98, 35);
+            this.adminLoginButton.TabIndex = 10;
+            this.adminLoginButton.Text = "登  录";
+            this.adminLoginButton.UseVisualStyleBackColor = true;
+            // 
+            // adminPwdTextBox
+            // 
+            this.adminPwdTextBox.Location = new System.Drawing.Point(331, 188);
+            this.adminPwdTextBox.Name = "adminPwdTextBox";
+            this.adminPwdTextBox.PasswordChar = '●';
+            this.adminPwdTextBox.Size = new System.Drawing.Size(169, 21);
+            this.adminPwdTextBox.TabIndex = 9;
+            // 
+            // adminNameTextBox
+            // 
+            this.adminNameTextBox.Location = new System.Drawing.Point(331, 135);
+            this.adminNameTextBox.Name = "adminNameTextBox";
+            this.adminNameTextBox.Size = new System.Drawing.Size(169, 21);
+            this.adminNameTextBox.TabIndex = 8;
+            // 
+            // adminPwdLabel
+            // 
+            this.adminPwdLabel.AutoSize = true;
+            this.adminPwdLabel.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.adminPwdLabel.Location = new System.Drawing.Point(270, 188);
+            this.adminPwdLabel.Name = "adminPwdLabel";
+            this.adminPwdLabel.Size = new System.Drawing.Size(56, 16);
+            this.adminPwdLabel.TabIndex = 7;
+            this.adminPwdLabel.Text = "密  码";
+            // 
+            // adminNameLabel
+            // 
+            this.adminNameLabel.AutoSize = true;
+            this.adminNameLabel.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.adminNameLabel.Location = new System.Drawing.Point(270, 135);
+            this.adminNameLabel.Name = "adminNameLabel";
+            this.adminNameLabel.Size = new System.Drawing.Size(56, 16);
+            this.adminNameLabel.TabIndex = 6;
+            this.adminNameLabel.Text = "用户名";
             // 
             // UserLogin
             // 
@@ -222,17 +222,17 @@
         private System.Windows.Forms.TabControl LoginPages;
         private System.Windows.Forms.TabPage UserPage;
         private System.Windows.Forms.TextBox userPwdTextBox;
-        private System.Windows.Forms.TextBox userAccountTextBox;
+        private System.Windows.Forms.TextBox userNameTextBox;
         private System.Windows.Forms.Label userPwdLabel;
-        private System.Windows.Forms.Label userAccountLabel;
+        private System.Windows.Forms.Label userNameLabel;
         private System.Windows.Forms.TabPage AdminPage;
         private System.Windows.Forms.Button userQuitButton;
         private System.Windows.Forms.Button userLoginButton;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button adminQuitButton;
+        private System.Windows.Forms.Button adminLoginButton;
+        private System.Windows.Forms.TextBox adminPwdTextBox;
+        private System.Windows.Forms.TextBox adminNameTextBox;
+        private System.Windows.Forms.Label adminPwdLabel;
+        private System.Windows.Forms.Label adminNameLabel;
     }
 }
